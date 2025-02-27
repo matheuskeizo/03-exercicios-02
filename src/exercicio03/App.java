f1f0f4d7e8d75b09e76f394d8a8583ba0630f264
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 11:47:11 -0300

    Enviando Arquivo

--- src/exercicio03/App.java
+++ src/exercicio03/App.java
@@ -1,2 +1,30 @@
-package exercicio03;public class App {
+package exercicio03;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        DecimalFormat fM = new DecimalFormat("0.00");
+        Ponto p1 = new Ponto();
+        Ponto p2 = new Ponto();
+
+        System.out.println("Informe o ponto :");
+        p1.x = e.nextInt();
+        p1.y = e.nextInt();
+        System.out.println();
+        System.out.println("Informe o ponto 2:");
+        p2.x = e.nextInt();
+        p2.y = e.nextInt();
+        System.out.println();
+        System.out.println("A distancia do p1 para p2:"+fM.format(p1.calculaDist(p2)));
+        System.out.println("A distancia do p2 para p1:"+fM.format(p2.calculaDist(p1)));
+        System.out.println();
+        System.out.println("A distancia dos pontos para origem:"+fM.format(p1.distOrig()));
+        System.out.println("A distancia dos pontos para origem:"+fM.format(p2.distOrig()));
+        System.out.println();
+        Ponto aux=p1.pontoMaisProx(p1,p2);
+        System.out.println("Ponto mais próximo da origem:"+fM.format(aux.formatPonto()));
+    }
 }
--- src/exercicio03/Ponto.java
+++ src/exercicio03/Ponto.java
@@ -1,11 +1,31 @@
 package exercicio03;
 
-public class Ponto1 {
+public class Ponto {
     int x;
     int y;
 
-    public int calculaDist(){
-        int distancia=0;
-
+    public double calculaDist(Ponto p) {
+        double distPont = 0;
+        distPont = Math.sqrt(Math.pow(x - p.x, 2) + Math.pow(y - p.y, 2));
+        return distPont;
     }
+
+    public double distOrig() {
+        Ponto origem = new Ponto();
+        return calculaDist(origem);
+    }
+
+    public String formatPonto() {
+        return "(" + x + "," + y + ")";
+    }
+
+    public Ponto pontoMaisProx(Ponto p1, Ponto p2) {
+        double dp1 = p1.distOrig();
+        double dp2 = p2.distOrig();
+        if (dp1 < dp2) {
+            return p1;
+        }
+        return p2;
+    }
+
 }
